d290c87f441d42657af20949ae6d53f25d3a01b5
mfd <123mufaddal@gmail.com>
2018-04-18 10:19:06 +0530

    updated Test for RouterScraper

--- WASPeopleTracker/src/test/java/org/mfd/was/peopletracker/TryRouterScraperService.java
+++ WASPeopleTracker/src/test/java/org/mfd/was/peopletracker/TryRouterScraperService.java
@@ -1,10 +1,14 @@
 package org.mfd.was.peopletracker;
 
 public class TryRouterScraperService {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		RouterScraper rs=RouterScraper.getScraper(0);
+		Thread t=new Thread(rs);
+		t.start();
+		Thread.sleep(1000*2);
 		
+		//t.interrupt();
 	}
 
 }
